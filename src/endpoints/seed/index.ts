7a6571ff5271e10af6a8dbe6ebff22cda0b8d296
warleon <51892390+warleon@users.noreply.github.com>
2026-07-29 21:14:38 -0500

    Update index.ts

--- src/endpoints/seed/index.ts
+++ src/endpoints/seed/index.ts
@@ -49,7 +49,7 @@ export const seed = async ({
       payload.updateGlobal({
         slug: global,
         data: {
-          navItems: [],
+          //navItems: [],
         },
         depth: 0,
         context: {
