b697ae5c4a75f252b35ba154bfe8011d87d8e1e2
warleon <51892390+warleon@users.noreply.github.com>
2026-08-08 11:05:12 -0500

    Update layout.tsx

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -23,8 +23,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
       <head>
         <InitTheme />
-        <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/rednati.ico" rel="icon" sizes="32x32" />
+        <link href="/rednati.svg" rel="icon" type="image/svg+xml" />
       </head>
       <body>
         <Providers>
