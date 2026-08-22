8c47f7fa5660ecca5b773debf3242687790479b7
warleon <51892390+warleon@users.noreply.github.com>
2026-08-22 00:51:33 -0500

    Update generateMeta.ts

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -27,8 +27,8 @@ export const generateMeta = async (args: {
   const ogImage = getImageURL(doc?.meta?.image)
 
   const title = doc?.meta?.title
-    ? doc?.meta?.title + ' | Payload Website Template'
-    : 'Payload Website Template'
+    ? doc?.meta?.title
+    : 'RedNatí - Red Nacional de Conservación de la Naturaleza'
 
   return {
     description: doc?.meta?.description,
