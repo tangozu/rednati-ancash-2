0417b7eee58bb2ed76f9d9fd7e8f25119b728c5b
warleon <51892390+warleon@users.noreply.github.com>
2026-08-26 10:48:29 -0500

    remove ununsed code

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,10 +3,8 @@ import path from 'path'
 import { buildConfig, PayloadRequest } from 'payload'
 import { fileURLToPath } from 'url'
 
-import { Categories } from './collections/Categories'
 import { Media } from './collections/Media'
 import { Pages } from './collections/Pages'
-import { Posts } from './collections/Posts'
 import { Users } from './collections/Users'
 import { Footer } from './Footer/config'
 import { Header } from './Header/config'
@@ -83,7 +81,7 @@ export default buildConfig({
       maxPoolSize: 5
     }
   }),
-  collections: [Pages, Posts, Media, Categories, Users],
+  collections: [Pages, Media, Users],
   cors: [getServerSideURL()].filter(Boolean),
   plugins: [
     ...plugins,
